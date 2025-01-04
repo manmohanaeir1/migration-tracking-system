f43c2cfd18dc11b36922960fae3e80fe71c40d23
Manmohan Aeir <manmohanaeir001110@gmail.com>
2025-01-04 17:33:07 +0545

    added

--- storage/framework/views/2fcc11f507cc5d97811e72915279b1fd035ac52b.php
+++
@@ -1,3 +0,0 @@
-
-<?php echo $__env->yieldContent('content'); ?>
-<?php /**PATH /media/manmohan/D25CA17B5CA15ACD/code&projectsDOC/2081/migration-tracking-system/resources/views/admin/content.blade.php ENDPATH**/ ?>
